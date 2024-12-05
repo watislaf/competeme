af9a415c3085f564801abf10a1e22d053f04fc6c
kdominiec <117099452+N3jm4n@users.noreply.github.com>
2024-12-05 18:29:56 +0100

    Added user search query

--- server/src/main/java/app/user/UserService.java
+++ server/src/main/java/app/user/UserService.java
@@ -3,6 +3,11 @@ package app.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.stream.Collectors;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +17,13 @@ public class UserService {
     public User getUserById(Integer userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with id: " + userId));
+    }
+
+    public List<UserSearchResponse> searchUsers(String query) {
+        Pageable pageable = PageRequest.of(0, 5); // Pierwsza strona, 5 wyników
+        return userRepository.findByNameContainingIgnoreCase(query, pageable).stream()
+                .map(user -> new UserSearchResponse(user.getId(), user.getName(), user.getImageUrl()))
+                .collect(Collectors.toList());
     }
 }
 
