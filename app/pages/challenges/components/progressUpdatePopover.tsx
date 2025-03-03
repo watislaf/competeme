22feff2fa72e742186e301af4638ae6fc69615b5
Pola Walczynska <pola.walczynska@gmail.com>
2025-03-03 13:08:56 +0100

    refactor progress update

--- app/pages/challenges/components/progressUpdatePopover.tsx
+++ app/pages/challenges/components/progressUpdatePopover.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   Popover,
   PopoverTrigger,
   PopoverContent,
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { ChallengeResponse } from "@/api/models";
-import { useUpdateProgressMutation } from "../hooks/useUpdateProgressMutation";
+import { ProgressEditor } from "./progressEditor";
 
 interface ProgressUpdatePopoverProps {
   userId: number;
@@ -22,71 +21,18 @@ export const ProgressUpdatePopover: React.FC<ProgressUpdatePopoverProps> = ({
     (participant) => participant.userId === userId,
   )?.progres;
 
-  const [newProgress, setNewProgress] = useState<number>(userProgress || 0);
-
-  const { mutate: updateProgress, error: updateError } =
-    useUpdateProgressMutation();
-
-  const [progressValues, setProgressValues] = useState<Record<number, number>>(
-    {},
-  );
-
-  const handleProgressChange = (id: number, value: number) => {
-    setProgressValues((prev) => ({ ...prev, [id]: value }));
-  };
-
-  const handleUpdateProgress = (challengeId: number) => {
-    if (progressValues[challengeId] !== undefined) {
-      updateProgress({
-        userId: Number(userId),
-        challengeId,
-        progress: progressValues[challengeId],
-      });
-    }
-  };
-
-  const handleAdd = () => {
-    const updatedValue = newProgress + 1;
-    setNewProgress(updatedValue);
-    handleProgressChange(challenge.id, updatedValue);
-  };
-
-  const handleSubstract = () => {
-    const updatedValue = Math.max(newProgress - 1, 0);
-    setNewProgress(updatedValue);
-    handleProgressChange(challenge.id, updatedValue);
-  };
-
-  const handleInputChange = (value: number) => {
-    setNewProgress(value);
-    handleProgressChange(challenge.id, value);
-  };
-
-  const handleClick = () => {
-    handleUpdateProgress(challenge.id);
-  };
-
   return (
     <Popover>
       <PopoverTrigger>
         <Button>Update Progress</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <div className="flex flex-col items-start space-y-2">
-          <Input
-            type="number"
-            value={newProgress}
-            onChange={(e) => handleInputChange(Number(e.target.value))}
-          />
-          <div className="flex space-x-2">
-            <Button onClick={handleAdd}>Add</Button>
-            <Button onClick={handleSubstract}>Subtract</Button>
-            <Button onClick={handleClick}>Update</Button>
-          </div>
-          {updateError && (
-            <p className="text-red-500 mt-2">Error: {updateError.message}</p>
-          )}
-        </div>
+        <ProgressEditor
+          initialValue={Number(userProgress)}
+          maxValue={challenge.goal}
+          userId={userId}
+          challengeId={challenge.id}
+        />
       </PopoverContent>
     </Popover>
   );
