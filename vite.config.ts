c764e4702ad945101fe42b07860d8357ad0bcaa2
watislaf <watislaf@gmail.com>
2025-03-29 22:49:12 +0100

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,11 @@ import { defineConfig } from "vite";
 import envOnly from "vite-env-only";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-import { vercelPreset } from "@vercel/remix/vite";
-
 export default defineConfig({
   plugins: [
     envOnly(),
     tsconfigPaths(),
     remix({
-      presets: [vercelPreset()],
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
